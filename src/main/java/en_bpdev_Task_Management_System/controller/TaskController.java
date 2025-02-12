39211c831a2d297f6a67ff199db5c1dded7c9616
Brenden Pulliam <bpgit503@gmail.com>
2025-02-12 20:37:10 +0100

    Completed the implementation of the Task Controller

--- src/main/java/en_bpdev_Task_Management_System/controller/TaskController.java
+++ src/main/java/en_bpdev_Task_Management_System/controller/TaskController.java
@@ -1,8 +1,11 @@
 package en_bpdev_Task_Management_System.controller;
 
+import en_bpdev_Task_Management_System.entity.Task;
 import en_bpdev_Task_Management_System.service.TaskService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -12,5 +15,30 @@ public class TaskController {
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        return taskService.updateTask(id, taskDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
     }
 }
