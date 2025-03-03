dd71c2f3944f4c76a3e2c9ff5623915f0d852894
Bren <bpgit503@gmail.com>
2025-03-03 11:17:32 +0100

    Refactored Controller class by updating certain methods to return HTTP responses and add due date field to task class

--- src/main/java/en_bpdev_Task_Management_System/controller/TaskController.java
+++ src/main/java/en_bpdev_Task_Management_System/controller/TaskController.java
@@ -2,6 +2,9 @@ package en_bpdev_Task_Management_System.controller;
 
 import en_bpdev_Task_Management_System.entity.Task;
 import en_bpdev_Task_Management_System.service.TaskService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,17 +31,28 @@ public class TaskController {
     }
 
     @PostMapping
-    public Task createTask(@RequestBody Task task) {
-        return taskService.createTask(task);
+    public ResponseEntity createTask(@RequestBody Task task) {
+
+        taskService.createTask(task);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", String.valueOf(task.getId()));
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+
     }
 
     @PutMapping("/{id}")
-    public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
-        return taskService.updateTask(id, taskDetails);
+    public ResponseEntity updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        taskService.updateTask(id, taskDetails);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTask(@PathVariable Long id) {
+    public ResponseEntity deleteTask(@PathVariable Long id) {
+
         taskService.deleteTask(id);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/en_bpdev_Task_Management_System/entity/Task.java
+++ src/main/java/en_bpdev_Task_Management_System/entity/Task.java
@@ -20,6 +20,7 @@ public class Task {
 
     private String title;
     private String description;
+    private  LocalDate dueDate;
     private boolean completed;
 
     private LocalDate createAt;
